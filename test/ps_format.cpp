d0d7dcaacb9d06eea0d3a97593cdb8eeb9651d87
민준 배 <mbae059@daum.net>
2022-03-13 20:52:32 +0900

    ps_format

---
+++ test/ps_format.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define INF 987654321
+#define p_q priority_queue
+using namespace std;
+typedef pair<int, int> pii;
+typedef long long ll;
+int T, N, K, M;
+//ofstream out("temp.txt");
+
+void Input() {
+}
+
+void Solution() {
+}
+
+void Solve() {
+	Input();
+	Solution();
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	Solve();
+}
