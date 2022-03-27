ef165e93d7acc12a527728f3b6ad1881acf48170
Minjun Bae <mbae059@daum.net>
2022-03-28 01:52:28 +0900

    my last work tonight

--- etc/ps_format.cpp
+++ etc/ps_format.cpp
@@ -12,9 +12,20 @@ void Input() {
 	cin >> N;
 }
 
+void Hanoi(int from, int by, int to, int N) {
+	if (N == 1) {
+		cout << from << " " << to << endl;
+	}
+	else {
+		Hanoi(from, to, by, N - 1);
+		cout << from << " " << to << endl;
+		Hanoi(by, from, to, N-1);
+	}
+}
+
 void Solution() {
 	if (N >= 5) cout << pow(2, N) - 1;
-
+	else Hanoi(1, 2, 3, N);
 }
 
 void Solve() {
